6645be31b9d3a57fc41b64bcaf797e3dea17c286
Sanskar <sanskarbhosale59@gmail.com>
2023-10-24 22:36:04 +0530

    solve x, y, z

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,68 @@
 class Chronometer {
-  constructor() {
+  constructor() { this.currentTime = 0;
+    this.intervalId = null;
+    this.splits = [];
     // ... your code goes here
   }
 
   start(callback) {
+    if (this.intervalId === null) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
     // ... your code goes here
   }
 
   getMinutes() {
+    const minutes = Math.floor(this.currentTime/60);
+    return minutes;
     // ... your code goes here
   }
 
   getSeconds() {
+    const seconds = (this.currentTime % 60 )
+    return seconds;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    var formattedNumber = ("0" + value).slice(-2);
+    const val = formattedNumber.toString();
+    return val;
     // ... your code goes here
   }
 
   stop() {
+    if (this.intervalId === null) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0;
+    this.splits = [];
+  }
     // ... your code goes here
   }
 
   split() {
+    const minute = this.getMinutes()
+    const minVal  = this.computeTwoDigitNumber(minute)
+
+    const second = this.getSeconds()
+    const secVal = this.computeTwoDigitNumber(second)
+
+    const formattedTime = `${minVal}:${secVal}`
+
+    return formattedTime;
+  
     // ... your code goes here
   }
-}
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
