7226bc0abb68470c08447b1c437e415042fe12eb
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-11-11 07:54:29 +0100

    feat: modify chronometer class for mlsecs function

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -8,25 +8,33 @@ class Chronometer {
     if (callback) {
       this.intervalId = setInterval(() => {
         callback();
-      }, 1000);
+      }, 10);
     }
   }
 
   getMinutes() {
-    const minutesPassed = Math.floor(this.currentTime / 60);
+    const minutesPassed = Math.floor(this.currentTime / 60000);
     return minutesPassed;
   }
 
   getSeconds() {
-    const secondsPassed = this.currentTime % 60;
+    const secondsPassed = Math.floor(this.currentTime / 1000) % 60;
     return secondsPassed;
+  }
+
+  getMilliseconds() {
+    const millisecondsPassed = Math.floor(this.currentTime % 1000);
+    return millisecondsPassed;
   }
 
   computeTwoDigitNumber(value) {
     const numberInStringFormat = `${value}`;
     if (numberInStringFormat.length === 1) {
-      const paddeddNumber = `0${numberInStringFormat.slice()}`;
+      const paddeddNumber = `0${numberInStringFormat}`;
       return paddeddNumber;
+    } else if (numberInStringFormat.length > 2) {
+      const [firstDigit, secondDigit] = numberInStringFormat;
+      return `${firstDigit}${secondDigit}`;
     }
     return numberInStringFormat;
   }
@@ -37,12 +45,14 @@ class Chronometer {
 
   reset() {
     this.currentTime = 0;
+    this.intervalId = null;
   }
 
   split() {
     const minutes = this.computeTwoDigitNumber(this.getMinutes());
     const seconds = this.computeTwoDigitNumber(this.getSeconds());
-    return `${minutes}:${seconds}`;
+    const milliseconds = this.computeTwoDigitNumber(this.getMilliseconds());
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
 }
 
