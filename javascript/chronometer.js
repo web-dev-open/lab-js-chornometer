272a92852e1407fc3a75d457abb34e6be4b5eff1
Bonthu Anusha <85819074+BonthuAnusha@users.noreply.github.com>
2023-11-28 15:32:09 +0530

    Updated

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,67 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime=0;
+    this.intervalId=null;
+    
+    // Bonus
+    this.milliseconds = 0;
+    this.milliIntevalId = null;
   }
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if(callback) callback();
+    }, 1000);
+
+    // Bonus
+    this.milliIntevalId = setInterval(() => {
+      this.milliseconds += 1;
+      if(callback) callback();
+    }, 10);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
+  // Bonus
+  getMilliseconds() {
+    return this.milliseconds % 100; 
+  }
+  
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const numberStr = String(value).padStart(2, '0');
+    return numberStr;
   }
 
   stop() {
     // ... your code goes here
+    learInterval(this.intervalId);
+    clearInterval(this.milliIntevalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.milliseconds = 0;
   }
 
   split() {
     // ... your code goes here
+     const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    //const milliseconds = this.computeTwoDigitNumber(this.getMilliseconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
