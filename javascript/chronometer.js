e3adfdaed49fed423dea27802652faf8559f02a9
Bonthu Anusha <85819074+BonthuAnusha@users.noreply.github.com>
2023-11-28 16:09:13 +0530

    Updated

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,72 +1,72 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-    this.currentTime=0;
-    this.intervalId=null;
-    
+    constructor() {
+      this.currentTime = 0;
+      this.intervalId = null;
+  
+      // Bonus
+      this.milliseconds = 0;
+      this.milliIntevalId = null;
+    }
+  
+    start(callback) {
+      this.intervalId = setInterval(() => {
+        this.currentTime += 1;
+        if(callback) callback();
+      }, 1000);
+  
+      // Bonus
+      this.milliIntevalId = setInterval(() => {
+        this.milliseconds += 1;
+        if(callback) callback();
+      }, 10);
+    }
+  
+    getMinutes() {
+      return Math.floor(this.currentTime / 60);
+    }
+  
+    getSeconds() {
+      return this.currentTime % 60;
+    }
+  
     // Bonus
-    this.milliseconds = 0;
-    this.milliIntevalId = null;
-  }
-
-  start(callback) {
-    // ... your code goes here
-    this.intervalId = setInterval(() => {
-      this.currentTime += 1;
-      if(callback) callback();
-    }, 1000);
-
+    getMilliseconds() {
+      return this.milliseconds % 100; 
+    }
+  
+    computeTwoDigitNumber(value) {
+      // Convert the number to string and pad it with a leading '0' if necessary
+      const numberStr = String(value).padStart(2, '0');
+      return numberStr;
+    }
+  
+    stop() {
+      clearInterval(this.intervalId);
+      clearInterval(this.milliIntevalId);
+    }
+  
+    reset() {
+      this.currentTime = 0;
+      this.milliseconds = 0;
+    }
+  
+    split() {
+      const minutes = this.computeTwoDigitNumber(this.getMinutes());
+      const seconds = this.computeTwoDigitNumber(this.getSeconds());
+      //const milliseconds = this.computeTwoDigitNumber(this.getMilliseconds());
+  
+      return `${minutes}:${seconds}`;
+    }
+  
     // Bonus
-    this.milliIntevalId = setInterval(() => {
-      this.milliseconds += 1;
-      if(callback) callback();
-    }, 10);
-  }
-
-  getMinutes() {
-    // ... your code goes here
-    return Math.floor(this.currentTime / 60);
-  }
-
-  getSeconds() {
-    // ... your code goes here
-    return this.currentTime % 60;
-  }
-
-  // Bonus
-  getMilliseconds() {
-    return this.milliseconds % 100; 
+    splitmilli() {
+      const milliseconds = this.computeTwoDigitNumber(this.getMilliseconds());
+      return `${milliseconds}`;
+    }
   }
   
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-    const numberStr = String(value).padStart(2, '0');
-    return numberStr;
+  // The following is required to make unit tests work.
+  /* Environment setup. Do not modify the below code. */
+  if (typeof module !== 'undefined') {
+    module.exports = Chronometer;
   }
-
-  stop() {
-    // ... your code goes here
-    learInterval(this.intervalId);
-    clearInterval(this.milliIntevalId);
-  }
-
-  reset() {
-    // ... your code goes here
-    this.currentTime = 0;
-    this.milliseconds = 0;
-  }
-
-  split() {
-    // ... your code goes here
-     const minutes = this.computeTwoDigitNumber(this.getMinutes());
-    const seconds = this.computeTwoDigitNumber(this.getSeconds());
-    //const milliseconds = this.computeTwoDigitNumber(this.getMilliseconds());
-    return `${minutes}:${seconds}`;
-  }
-}
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
-}
